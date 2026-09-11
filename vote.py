e06156fda29b7065afa27ec40d8b64051aadec25
supritauppar00-max <supritauppar00@gmail.com>
2026-09-11 02:15:56 -0700

    enhance voter eligibility meassage

--- vote.py
+++ vote.py
@@ -2,5 +2,7 @@ age = int(input("enter your age:"))
 
 if age>=18;
     print("your are eligible to vote.")
+    print("you can register as a voter.")
 else:
     print("your are not eligible to vote.")
+    print("you must be at list 18 for vote.")
